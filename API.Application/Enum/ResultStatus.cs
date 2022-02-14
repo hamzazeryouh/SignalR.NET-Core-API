86a447730a9d6d64b314a40e56cdc9ccb345730a
hamza zeryouh <hamza.zeryouh.ajicreative.club>
2022-02-14 15:29:57 +0100

    add page Resiulte PagedResult , and Result

---
+++ API.Application/Enum/ResultStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Application
+{
+    /// <summary>
+    /// describe the status of an operation result
+    /// </summary>
+    public enum ResultStatus
+    {
+        /// <summary>
+        /// the operation has been succeed
+        /// </summary>
+        Succeed,
+
+        /// <summary>
+        /// operation has been failed
+        /// </summary>
+        Failed
+    }
+}
---
+++ API.Application/Enum/SortDirection.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Application
+{
+    /// <summary>
+    /// the order by type
+    /// </summary>
+    public enum SortDirection
+    {
+        /// <summary>
+        /// OrderBy Descending
+        /// </summary>
+        Desc,
+
+        /// <summary>
+        /// OrderBy Ascending
+        /// </summary>
+        Asc
+    }
+}
