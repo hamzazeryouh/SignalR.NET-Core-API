2cd5473e7820878117e6ab04ae3a09abce6d754a
hamza zeryouh <hamza.zeryouh.ajicreative.club>
2022-02-14 12:09:07 +0100

    add Interface Controller With Logg

--- SignalR.NET Core API/Controller/BaseController.cs
+++ SignalR.NET Core API/Controller/BaseController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,9 +7,17 @@ using System.Threading.Tasks;
 
 namespace SignalR.NET_Core_API.Controller
 {
-    public class BaseController : ControllerBase
+    public class BaseController<T> : ControllerBase
     {
-        public BaseController()
-        { }
+        protected readonly ILogger<T> Logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseApiController{T}"/> class.
+        /// </summary>
+        /// <param name="factory">The factory.</param>
+        protected BaseController(ILoggerFactory factory)
+        {
+            Logger = factory.CreateLogger<T>();
+        }
     }
 }
--- SignalR.NET Core API/Controller/ChatController.cs
+++ SignalR.NET Core API/Controller/ChatController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace SignalR.NET_Core_API.Controller
 {
-    public class ChatController : BaseController
+    public class ChatController : BaseController<ChatController>
     {
-       
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        protected ChatController(ILoggerFactory factory) : base(factory)
+        {
+        }
+
     }
 }
