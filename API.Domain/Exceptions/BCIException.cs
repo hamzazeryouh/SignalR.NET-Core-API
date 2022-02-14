e1f59247eb913a86b77347a7fd657c93e6afee32
hamza zeryouh <hamza.zeryouh.ajicreative.club>
2022-02-14 15:30:24 +0100

    Add Exception

---
+++ API.Domain/Exceptions/BCIException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace API.Domain.Exceptions
+{
+    /// <summary>
+    /// the base exception Class for all Application Exceptions
+    /// </summary>
+    [Serializable]
+    public class BCIException : Exception
+    {
+        public BCIException() { }
+
+        public BCIException(string message) 
+            : base(message) { }
+
+        public BCIException(string message, Exception inner) 
+            : base(message, inner) { }
+
+        protected BCIException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) 
+            : base(info, context) { }
+    }
+}
