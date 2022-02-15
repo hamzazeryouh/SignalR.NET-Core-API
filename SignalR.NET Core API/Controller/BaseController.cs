34b75b4f92761f313576a86ff354272d90522d26
hamza zeryouh <hamza.zeryouh.ajicreative.club>
2022-02-15 11:16:30 +0100

    fix base controller

--- SignalR.NET Core API/Controller/BaseController.cs
+++ SignalR.NET Core API/Controller/BaseController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Application;
+using API.Application.Generals;
+using API.Common.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -7,17 +12,66 @@ using System.Threading.Tasks;
 
 namespace SignalR.NET_Core_API.Controller
 {
+    [ApiController]
+    [Authorize]
     public class BaseController<T> : ControllerBase
     {
-        protected readonly ILogger<T> Logger;
+
+ 
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="BaseApiController{T}"/> class.
+        /// create an instant of <see cref="BaseController"/>
         /// </summary>
-        /// <param name="factory">The factory.</param>
-        protected BaseController(ILoggerFactory factory)
+        public BaseController(ILoggerFactory factory)
+        { }
+
+        /// <summary>
+        /// this method is used to return the proper action result type
+        /// </summary>
+        /// <typeparam name="TResult">the type of the Result being processed</typeparam>
+        /// <param name="taskResult">the result to process</param>
+        /// <returns>the proper Action Result base on the passed in Result</returns>
+        public async Task<ActionResult<TResult>> ActionResultForAsync<TResult>(Task<TResult> taskResult)
+            where TResult : Result => ActionResultFor(await taskResult);
+
+        /// <summary>
+        /// this method is used to return the proper action result type
+        /// </summary>
+        /// <typeparam name="TResult">the type of the Result being processed</typeparam>
+        /// <param name="result">the result to process</param>
+        /// <returns>the proper Action Result base on the passed in Result</returns>
+        public ActionResult<TResult> ActionResultFor<TResult>(TResult result)
+            where TResult : Result
         {
-            Logger = factory.CreateLogger<T>();
+            // the operation has failed
+            if (result.Status == ResultStatus.Failed)
+            {
+                // something went wrong (exception)
+                if (result.HasError)
+                    return StatusCode(500, result);
+
+                // result not found
+                if (!result.HasValue || result.MessageCode == MsgCode.OperationFailedNotFound.ToString())
+                    return NotFound(result);
+
+                // user is not authorized
+                if (result.MessageCode.Equals(MsgCode.Unauthorized.ToString()))
+                    return StatusCode(StatusCodes.Status403Forbidden, result);
+
+                //if nothing bad request
+                return BadRequest(result);
+            }
+
+            // all set, return the operation result
+            return Ok(result);
         }
+
+        /// <summary>
+        /// get the base URL, ex: "http://localhost:5000"
+        /// </summary>
+        /// <returns>the base URL</returns>
+        protected string GetBaseUrl()
+            => $"{Request.Scheme}://{Request.Host.ToUriComponent()}{Request.PathBase.ToUriComponent()}";
     }
+
 }
