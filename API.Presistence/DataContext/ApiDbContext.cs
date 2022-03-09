a0eb314c1f2c0505fa3a0d21657b68b37e9719f1
hamza zeryouh <hamza.zeryouh.ajicreative.club>
2022-03-09 12:54:49 +0100

    add Identiity with class user

---
+++ API.Presistence/DataContext/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace API.Presistence
+{
+    public class ApiDbContext : IdentityDbContext<User>
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
